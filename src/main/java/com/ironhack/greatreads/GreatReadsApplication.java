cb7f63a3dcba492a023fd540e94b1c1c0b03e5fd
evagapi <87602505+evagapi@users.noreply.github.com>
2024-04-13 14:25:33 +0200

    chore: initial commit

---
+++ src/main/java/com/ironhack/greatreads/GreatReadsApplication.java
@@ -0,0 +1,14 @@
+package com.ironhack.greatreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatReadsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello mundo");
+		SpringApplication.run(GreatReadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/greatreads/GreatReadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.greatreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatReadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
