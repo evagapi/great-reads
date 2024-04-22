02d0a292b83eb5bbc317145943247d1f0a1aa995
evagapi <87602505+evagapi@users.noreply.github.com>
2024-04-22 20:22:40 +0200

    feat: add Book, Author and Genre repositories tests

---
+++ src/test/java/com/ironhack/greatreads/repository/book/AuthorRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.ironhack.greatreads.repository.book;
+
+import com.ironhack.greatreads.model.book.Author;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AuthorRepositoryTest {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    private Author author;
+
+    @BeforeEach
+    void setUp() {
+        author = new Author("Jane", "Austen");
+        authorRepository.save(author);
+    }
+
+    @AfterEach
+    void tearDown() {
+        authorRepository.deleteAll();
+    }
+
+    @Test
+    void findByLastNameTest() {
+        Optional<Author> $author = authorRepository.findByLastName("Austen");
+        assertTrue($author.isPresent());
+        assertEquals("Austen", $author.get().getLastName());
+    }
+
+    @Test
+    void findByFirstNameTest() {
+        Optional<Author> $author = authorRepository.findByFirstName("Jane");
+        assertTrue($author.isPresent());
+        assertEquals("Jane", $author.get().getFirstName());
+    }
+
+    @Test
+    void findByIdTest() {
+        Optional<Author> $author = authorRepository.findById(author.getId());
+        assertTrue($author.isPresent());
+        assertEquals(author.getId(), $author.get().getId());
+    }
+
+    @Test
+    void deleteAuthorTest() {
+        authorRepository.delete(author);
+        Optional<Author> $author = authorRepository.findById(author.getId());
+        assertFalse($author.isPresent());
+    }
+}
---
+++ src/test/java/com/ironhack/greatreads/repository/book/BookRepositoryTest.java
@@ -0,0 +1,86 @@
+package com.ironhack.greatreads.repository.book;
+
+import com.ironhack.greatreads.model.book.Author;
+import com.ironhack.greatreads.model.book.Book;
+import com.ironhack.greatreads.model.book.Genre;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BookRepositoryTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Autowired
+    GenreRepository genreRepository;
+
+    @Autowired
+    AuthorRepository authorRepository;
+
+    Genre genre;
+    Author author;
+    Book book;
+
+    @BeforeEach
+    void setUp() {
+        genre = new Genre("Sci-fi");
+        author = new Author("Brandon", "Sanderson");
+        book = new Book();
+        book.setTitle("El imperio final");
+        book.setNumberOfPages(672);
+        book.setIsbn("9788417347291");
+        book.setLanguage("Spanish");
+        book.setPublisher("Nova");
+        book.setGenre(genre);
+        book.setAuthor(author);
+
+        genreRepository.save(genre);
+        authorRepository.save(author);
+        bookRepository.save(book);
+    }
+
+    @AfterEach
+    void tearDown() {
+        bookRepository.deleteAll();
+        genreRepository.deleteAll();
+        authorRepository.deleteAll();
+    }
+
+    @Test
+    void findBookByTitleTest() {
+        Optional<Book> $book = bookRepository.findBookByTitle("El imperio final");
+        assertTrue($book.isPresent());
+        assertEquals("El imperio final", $book.get().getTitle());
+    }
+
+    @Test
+    void findBookByAuthorLastNameTest() {
+        Optional<Book> $book = bookRepository.findBookByAuthorLastName(author.getLastName());
+        assertTrue($book.isPresent());
+        assertEquals("Sanderson", $book.get().getAuthor().getLastName());
+    }
+
+    @Test
+    void findBookByGenreTest() {
+        Optional<Book> $book = bookRepository.findBookByGenreName(genre.getName());
+        assertTrue($book.isPresent());
+        assertEquals("Sci-fi", $book.get().getGenre().getName());
+    }
+
+    @Test
+    void deleteBookTest() {
+        bookRepository.delete(book);
+        Optional<Book> $book = bookRepository.findById(book.getId());
+        assertFalse($book.isPresent());
+    }
+}
