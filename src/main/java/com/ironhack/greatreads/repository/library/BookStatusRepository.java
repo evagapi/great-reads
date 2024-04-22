9ca9138b93c1766b2a0dfd36d80eae8d653420b5
evagapi <87602505+evagapi@users.noreply.github.com>
2024-04-22 20:56:12 +0200

    test: add BookStatus repository test

--- src/main/java/com/ironhack/greatreads/repository/library/BookStatusRepository.java
+++ src/main/java/com/ironhack/greatreads/repository/library/BookStatusRepository.java
@@ -4,6 +4,9 @@ import com.ironhack.greatreads.model.library.BookStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookStatusRepository extends JpaRepository<BookStatus, Integer> {
+    Optional<BookStatus> findById(int id);
 }
