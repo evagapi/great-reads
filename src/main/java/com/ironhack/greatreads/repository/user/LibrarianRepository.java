1d71cd6d3b445d90f84402c425d28273597e2adf
evagapi <87602505+evagapi@users.noreply.github.com>
2024-04-23 11:20:42 +0200

    test: add Librarian repository tests

--- src/main/java/com/ironhack/greatreads/repository/user/LibrarianRepository.java
+++ src/main/java/com/ironhack/greatreads/repository/user/LibrarianRepository.java
@@ -4,6 +4,10 @@ import com.ironhack.greatreads.model.user.Librarian;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LibrarianRepository extends JpaRepository<Librarian, Integer> {
+    Optional<Librarian> findById(int id);
+    Optional<Librarian> findByUserName(String userName);
 }
---
+++ src/test/java/com/ironhack/greatreads/repository/user/LibrarianRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.ironhack.greatreads.repository.user;
+
+import com.ironhack.greatreads.model.user.Librarian;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LibrarianRepositoryTest {
+
+    @Autowired
+    LibrarianRepository librarianRepository;
+
+    Librarian librarian;
+
+    @BeforeEach
+    void setUp() {
+        librarian = new Librarian("Madam Pince", "madam_pince", "m.pince@hogwarts.edu");
+        librarianRepository.save(librarian);
+    }
+
+    @AfterEach
+    void tearDown() {
+        librarianRepository.deleteAll();
+    }
+
+    @Test
+    void findByIdTest() {
+        Optional<Librarian> $librarian = librarianRepository.findById(librarian.getId());
+        assertTrue($librarian.isPresent());
+        assertEquals(librarian.getId(), $librarian.get().getId());
+    }
+
+    @Test
+    void findByUserNameTest() {
+        Optional<Librarian> $librarian = librarianRepository.findByUserName(librarian.getUserName());
+        assertTrue($librarian.isPresent());
+        assertEquals(librarian.getUserName(), $librarian.get().getUserName());
+    }
+}
