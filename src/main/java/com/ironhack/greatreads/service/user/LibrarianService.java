def947a40a7788dff7c45ff1c8a7cb1fab40764f
evagapi <87602505+evagapi@users.noreply.github.com>
2024-04-30 19:07:17 +0200

    feat(librarian): add update, delete and get by id functionalities

--- src/main/java/com/ironhack/greatreads/service/user/LibrarianService.java
+++ src/main/java/com/ironhack/greatreads/service/user/LibrarianService.java
@@ -3,7 +3,9 @@ package com.ironhack.greatreads.service.user;
 import com.ironhack.greatreads.model.user.Librarian;
 import com.ironhack.greatreads.repository.user.LibrarianRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -15,5 +17,28 @@ public class LibrarianService {
 
     public List<Librarian> getAllLibrarians() { return librarianRepository.findAll(); }
 
+    public Librarian getLibrarianById(int id) {
+        return librarianRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Librarian not found"));
+    };
+
     public Librarian addNewLibrarian(Librarian librarian) { return librarianRepository.save(librarian); }
+
+    public Librarian updateLibrarian(int id, Librarian librarian) {
+        Librarian existingLibrarian = librarianRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Librarian not found"));
+
+        if (librarian.getName() != null) existingLibrarian.setName(librarian.getName());
+        if (librarian.getUserName() != null) existingLibrarian.setUserName(librarian.getUserName());
+        if (librarian.getUserName() != null) existingLibrarian.setUserName(librarian.getUserName());
+        if (librarian.getEmail() != null) existingLibrarian.setEmail(librarian.getEmail());
+
+        librarianRepository.save(existingLibrarian);
+
+        return existingLibrarian;
+    }
+
+    public void deleteLibrarian(int id) {
+        Librarian existingLibrarian = librarianRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Librarian not found"));
+
+        librarianRepository.delete(existingLibrarian);
+    }
 }
